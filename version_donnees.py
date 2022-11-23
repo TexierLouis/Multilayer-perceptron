35f91fbd6c516ddb7eed4e526a7ccef221b66160
TexierLouis <sititxr@gmail.com>
2022-11-23 16:50:12 +0100

    Add of GridSearchCV

--- version_donnees.py
+++ version_donnees.py
@@ -7,6 +7,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import LeaveOneOut
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
 from matplotlib import pyplot as plt
 from matplotlib.colors import ListedColormap
@@ -22,7 +23,7 @@ data_raw_transposed = data_raw.T
 
 ### Visualisation of raw data
 
-''' A tester au CREMI
+''' A tester au CREMI NE MARCHE PAS CAR 50x50
 df = Data_Pam.iloc[:,2:]
 features = list(df)
 df = Data_Pam.iloc[:,1:]
@@ -67,21 +68,25 @@ score = clf.score(Data_test, label_test)
 
 ### Use of LeaveOneOut, use each data as test only one time and train with others, allows to give accuracy for short data range
 
+
 target = {'Normal': 0, 'Basal': 1, 'Her2': 2, 'LumA': 3, 'LumB': 4}
 label_target = np.zeros(len(label))
 for i in range (len(label)) :
     label_target[i] = target[label[i]]
 
-def train_model(param) :
+def train_model(layer, tols, epochs) :
     loo = LeaveOneOut()
     results = np.zeros(len(data))
     for train, test in loo.split(data):
-        clf = MLPClassifier(random_state=1, max_iter=300).fit(data[train], label_target[train])
+        clf = MLPClassifier(hidden_layer_sizes=layer, random_state=1, max_iter=epochs, tol = tols ).fit(data[train], label_target[train])
         results[test] = clf.predict(data[test])
-    print ("Accuracy=%f%%" % (accuracy_score(label_target, results)*100))
+        print(clf.loss_curve_)
+    return (accuracy_score(label_target, results)*100)
+
 
 ### Visualisation data and MLPClassifier
 
+'''
 Data_train, Data_test, label_train, label_test = train_test_split(data, label_target, stratify=label, random_state=1)
 figure = plt.figure(figsize=(17, 9))
 i = 1
@@ -93,3 +98,50 @@ ax.scatter(Data_train[:, 0], Data_train[:, 1], c=label_train, cmap=cm_bright)
 
 ax.scatter(Data_test[:, 0], Data_test[:, 1], c=label_test, cmap=cm_bright, alpha=0.6)
 plt.show()
+'''
+
+### Training with different parameters
+
+
+layer = [(25,1),(50,1),(25,10),(50,10)]
+tols = [10e-4, 10e-5, 10e-3, 10e-2]
+epochs = [20,50,100]
+Accuracy = []
+
+for i in layer :
+    Accuracy.append(train_model(i, tols[0], epochs[0]))
+
+for i in tols :
+    Accuracy.append(train_model(layer[0], i, epochs[0]))
+
+for i in epochs :
+    Accuracy.append(train_model(layer[0], tols[0], i))
+
+print("Each parameters tested")
+
+### Update with GridSeachCV
+
+'''
+print("Starting searching which are the best parameters")
+
+loo = LeaveOneOut()
+results = np.zeros(len(data))
+for train, test in loo.split(data):
+    clf = GridSearchCV(
+        MLPClassifier(),
+        param_grid= {
+            'hidden_layer_sizes': layer,
+            'tol' : tols,
+            'max_iter' : epochs
+            },
+        refit='True',
+        cv=2,
+        n_jobs=-1,
+    )
+    clf.fit(data[train], label_target[train])
+    results[test] = clf.predict(data[test])
+print ("Accuracy=%f%%" % (accuracy_score(label_target, results)*100))
+'''
+
+
+
